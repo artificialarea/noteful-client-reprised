8b9b39308d5c1671b280f1aeb2001e9dab0e8671
Sacha Sedriks <sachased@gmail.com>
2020-12-21 19:03:21 -0800

    Update heroku db API_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     // API_ENDPOINT: 'http://localhost:9090'                            // JSON Server
     // API_ENDPOINT: 'http://localhost:8000'                            // Noteful API DB localhost
-    API_ENDPOINT: 'https://powerful-hollows-76486.herokuapp.com'        // Noteful API DB heroku
+    API_ENDPOINT: 'https://noteful-db.herokuapp.com'        // Noteful API DB heroku
 }
